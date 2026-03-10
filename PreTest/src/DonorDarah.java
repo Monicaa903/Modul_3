f0b7654b9d5f1024228b01569a3f8ac2dc2e164c
Monicaa903 <siscamonica694@gmail.com>
2026-03-10 07:32:51 +0700

    pretest

---
+++ PreTest/src/DonorDarah.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DonorDarah {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("~~~ SISTEM PENGECEKAN DONOR DARAH ~~~");
+
+        System.out.print("Usia Anda : ");
+        int usia = input.nextInt();
+
+        System.out.print("Berat Badan (kg) : ");
+        float beratBadan = input.nextFloat();
+
+        System.out.print("Kadar Hemoglobin : ");
+        float hb = input.nextFloat();
+
+        System.out.println("\n~~~ Hasil Evaluasi ~~~");
+
+        if (usia >= 17) {
+            if (beratBadan >= 45.0f) {
+                if (hb >= 13.5f) {
+                    System.out.println("Selamat! Anda bisa menjadi donor darah.");
+                } else {
+                    System.out.println("Maaf, Anda Tidak Bisa Mendonorkan Darah , Hemoglobin minimal 13.5 g/dL.");
+                }
+            } else {
+                System.out.println("Maaf, Anda Tidak Bisa Mendonorkan Darah. Berat badan minimal 45 kg.");
+            }
+        } else {
+            System.out.println("Maaf, Tidak Bisa Usia minimal 17 tahun.");
+        }
+
+        input.close();
+    }
+}
